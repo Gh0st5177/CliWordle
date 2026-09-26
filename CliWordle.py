aa8b915756144b2de1a9ed035dacf5bf11499fab
Gh0st <gh0stsouls.py@gmail.com>
2026-09-26 15:24:15 +0100

    Bug fixes and Code Cleaning

--- CliWordle.py
+++ CliWordle.py
@@ -1,15 +1,6 @@
+#--Imports--#
 import os
 import random
-
-"""
-Process:
-    Clear Screen
-    Pick a word
-    Allow player to guess: Check if guess is 5 letters else ask again
-    Print the guess and check it changing the color of the text just like the real wordle
-    allow players to guess again
-    break cycle if the guess is correct or already guessed 6 times
-"""
 #--Misc-Variables--#
 guessCount=0
 correct=False
@@ -18,14 +9,12 @@ os.system("clear")
 #--Pick-A-Word--#
 words=open("words.txt","rt").read().splitlines()
 word=random.choice(words).lower()
-listedWord=[]
-for i in word:  listedWord.append(i)
+word="annex"
 #--Guess-Function--#
 def Guess():
     global guessCount
     global word
     global correct
-    global listedWord
     #--Creating-The-Guess--#
     while True:
         guess=input()
@@ -36,19 +25,17 @@ def Guess():
     guessCount+=1
     #--Processing-The-Guess--#
     print("\033[F",end="")
-    listedGuess=[]
-    for i in guess: listedGuess.append(i)
-    for i in listedGuess:
-        if i not in listedWord:
-            print(f"\033[91m{i}\033[0m",end="")
-        elif i in listedWord and listedWord.index(i)==listedGuess.index(i):
-            print(f"\033[92m{i}\033[0m",end="")
+    for i in range(len(guess)):
+        if guess[i] not in word:
+            print(f"\033[31m{guess[i]}\033[0m",end="")
+        elif guess[i]==word[i]:
+            print(f"\033[32m{guess[i]}\033[0m",end="")
         else:
-            print(f"\033[93m{i}\033[0m",end="")
+            print(f"\033[33m{guess[i]}\033[0m",end="")
     if guess==word:
           correct=True
     print()
 while guessCount<6 and correct==False:
     Guess()
-if guessCount==6:
+if guessCount==6 and correct==False:
     print("The word was:",word)
